15f6b8944f22df0443bd0b5378ca66171c238413
chloebates4 <chloe.bates4@gmail.com>
2020-04-25 16:37:57 -0700

    added netbeans project

---
+++ DistributedChat/src/distributedchat/ReceiverWorker.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package distributedchat;
+
+/**
+ *
+ * @author Chloe Bates, Sam Gilb, & Colton Spector
+ */
+public class ReceiverWorker {
+    
+}
